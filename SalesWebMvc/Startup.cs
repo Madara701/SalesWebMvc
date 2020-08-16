fc0995a72c69130912b059509d39cee96f8857f5
Madara701 <matos701@gmail.com>
2020-08-16 13:01:14 -0300

    populando DB

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -35,15 +35,18 @@ namespace SalesWebMvc
 
             services.AddDbContext<SalesWebMvcContext>(options => options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder => 
                 builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
             //Conexão com TSQL
             //services.AddDbContext<SalesWebMvcContext>(options =>
             //    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else {
                 app.UseExceptionHandler("/Home/Error");
