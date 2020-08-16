86da14d01a379c8ea2ea49f6d8eb0fea910efaf3
Madara701 <matos701@gmail.com>
2020-08-15 22:54:25 -0300

    primeira Migration

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,18 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index() {
+            
             return View();
         }
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
+            ViewData["email"] = "matos701@gmail.com";
+
 
             return View();
         }
